f73c7f6e848bd7e059b53b6a3332d2c031dc2de0
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-09-01 11:26:28 +0200

    Play and pause functionality achieved

--- web.js
+++ web.js
@@ -1,11 +1,47 @@
 var exec = require('child_process').exec,
 	child,
-	app = require('express')();
+	app = require('express')(),
+	port = 3000,
+	prefix = 'tell application "Spotify" to ';
 
+function osascript(command, callback){
+	"use strict";
+
+	callback = callback || function(){};
+
+	exec("osascript -e '" + prefix + command + "'", callback);
+}
+
+function getNameAndArtist(callback){
+	osascript('name of current track', function(){
+		var song = arguments[1];
+		osascript('artist of current track', function(){
+			var artist = arguments[1];
+			callback('Now playing ' + song + ' by ' + artist + '.');
+		});
+	});
+}
+
+// Routing
 app.get('/', function(req, res){
-	exec('osascript -e \'tell application "Spotify" to playpause\'');
-	res.send('Hello World');
+	res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.get('/play', function(req, res){
+	osascript('playpause', function(){
+		osascript('player state', function(){
+			console.log(arguments);
+			if( arguments[1] === 'playing\n' ){
+				getNameAndArtist(function(nameAndArtist){
+					res.send(nameAndArtist);
+				});
+			}
+			else {
+				res.send('Paused');
+			}
+		});
+	});
+});
+
+app.listen(port);
+console.log('Listening on port ' + port);
