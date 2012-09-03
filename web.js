13ea5afeaa2ccb4eb4017569b9a801e970312676
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-09-03 23:57:54 +0200

    Major performance upgrade by concatenating osascripts. All functions now working again.

--- web.js
+++ web.js
@@ -5,17 +5,28 @@ var exec = require('child_process').exec,
 function osascript(){
 	"use strict";
 	// args: command 1, command n, ..., callback
-	var command = 'osascript -e \'tell application "Spotify"\' ',
-		end = "-e 'end tell' -e 'return var'",
+	var command = 'osascript -e \'set var to ""\' -e \'tell application "Spotify"\' ',
+		end = "-e 'end tell'",
 		l = arguments.length,
 		callback = arguments[--l] || function(){},
-		i;
+		i,
+		notSet = 0;
 
 	for( i = 0; i < l; i++ ){
-		command += "-e 'set var to " + (i === 0 ? '' : 'var & ";;;" & ') + arguments[i] + "' ";
+		command += "-e '";
+
+		if( arguments[i].indexOf('set') === -1 ){
+			command += "set var to var & " + ( notSet === 0 ? '' : '";;;" & ' ) + arguments[i];
+			notSet++;
+		}
+		else {
+			command += arguments[i]
+		}
+
+		command += "' ";
 	}
 
-	command += end;
+	command += end + ( notSet === 0 ? '' :  " -e 'return var'" );
 
 	exec(command, function(err, result){
 		if( !err ){
@@ -28,55 +39,46 @@ function osascript(){
 	});
 }
 
-function getNameAndArtist(callback){
-	osascript('name of current track', function(){
-		var song = arguments[1];
-		osascript('artist of current track', function(){
-			var artist = arguments[1];
-			callback('Now playing ' + song + ' by ' + artist + '.');
-		});
-	});
-}
-
-function sendNameAndArtist(res, error){
-	isPlaying(function(isPlaying){
-		if( isPlaying ){
-			getNameAndArtist(function(nameAndArtist){
-				res.send(nameAndArtist);
-			});
-		}
-		else {
-			res.send(error || 'Not playing.');
-		}
-	});
-}
-
-function isPlaying(callback){
-	osascript('player state', function(){
-		callback(arguments[1] === 'playing\n');
-	});
-}
 
 // Routing
+
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/index.html');
 });
 
 app.get('/play', function(req, res){
-	osascript('playpause', function(){
-		sendNameAndArtist(res, 'Paused');
+	osascript('playpause', 'player state', 'name of current track', 'artist of current track', function(){
+		if( arguments[1] === 'playing' ){
+			res.send('Now playing ' + arguments[2] + ' by ' + arguments[3] + '.');
+		}
+		else if( arguments[1] === 'paused' ){
+			res.send('You paused in the middle of ' + arguments[2] + ' by ' + arguments[3] + '! :O');
+		}
+		else {
+			res.send('Nothing is playing...');
+		}
 	});
 });
 
 app.get('/next', function(req, res){
-	osascript('next track', function(){
-		sendNameAndArtist(res, 'No song is playing.');
+	osascript('next track', 'player state', 'name of current track', 'artist of current track', function(){
+		if( arguments[1] === 'playing' ){
+			res.send('Now playing ' + arguments[2] + ' by ' + arguments[3] + '.');
+		}
+		else {
+			res.send('Nothing is playing anymore... Guess this is the end of the road.');
+		}
 	});
 });
 
 app.get('/prev', function(req, res){
-	osascript('previous track', function(){
-		sendNameAndArtist(res, 'No song is playing.');
+	osascript('previous track', 'player state', 'name of current track', 'artist of current track', function(){
+		if( arguments[1] === 'playing' ){
+			res.send('Now playing ' + arguments[2] + ' by ' + arguments[3] + '.');
+		}
+		else {
+			res.send('Whoaa! You backed so fast nothing is playing anymore... :(');
+		}
 	});
 });
 
@@ -86,27 +88,27 @@ app.get('/get/:property', function(req, res){
 	switch( property ){
 		case 'state':
 			osascript('player state', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		case 'position':
 			osascript('player position', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		case 'volume':
 			osascript('sound volume', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		case 'name':
 			osascript('name of current track', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		case 'artist':
 			osascript('artist of current track', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		//## I need to figure out how to stream the response from 
@@ -117,21 +119,21 @@ app.get('/get/:property', function(req, res){
 		break;*/
 		case 'album':
 			osascript('album of current track', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		case 'duration':
 			osascript('duration of current track', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		case 'url':
 			osascript('spotify url of current track', function(){
-				res.send(arguments[1]);
+				res.send(arguments[0]);
 			});
 		break;
 		default:
-			res.send(404, 'Unknown property');
+			res.send(404, 'Que?');
 		break;
 	}
 });
@@ -143,28 +145,42 @@ app.get('/set/:property/:value', function(req, res){
 	switch( property ){
 		case 'state':
 			if( value === 'play' ){
-				osascript('play', function(){
-					sendNameAndArtist();
-				});	
+				osascript('play', 'player state', 'name of current track', 'artist of current track', function(){
+					if( arguments[1] === 'playing' ){
+						res.send('Now dancing to ' + arguments[2] + ' by ' + arguments[3] + '.');
+					}
+					else {
+						res.send('Because of technical problems we only offer the following a capella song: "Bä bää vita lamm, har du någon ull? [---]"');
+					}
+				});
 			}
 			else if( value === 'pause' ){
-				osascript('pause', function(){
-					res.send('Paused');
-				});	
-			}
-			else {
-				res.send(400, 'Unsupported state');
-			}
-		break;
-		case 'position':
-			if( !isNaN(value) ){
-				osascript('set player position to ' + value, function(){
-					//## When caching is implemented improvement can be done here by checking if the position exists
-					res.send('Player position set to ' + value);
+				osascript('pause', 'player state', 'name of current track', 'artist of current track', function(){
+					if( arguments[1] === 'paused' ){
+						res.send('You paused in the middle of ' + arguments[2] + ' by ' + arguments[3] + '! :O');
+					}
+					else {
+						res.send('Hey, I\'m not playing!');
+					}
 				});
 			}
 			else {
-				res.send(400, 'Invalid timestamp');
+				res.send(400, 'I can\'t do that!');
+			}
+		break;
+		case 'position':
+			if( !isNaN(value) && value >= 0 ){
+				osascript('set player position to ' + value, 'duration of current track', function(){
+					if( value < arguments[1] ){
+						res.send('Yeah! I\'ve always loved the part at ' + value + ' seconds!');
+					}
+					else {
+						res.send('I know this song is way too long, but not that long!');
+					}
+				});
+			}
+			else {
+				res.send(400, 'That\'s not a knife... THIS is a knife.');
 			}
 		break;
 		case 'volume':
@@ -173,12 +189,18 @@ app.get('/set/:property/:value', function(req, res){
 					res.send('Volume set to ' + value);
 				});
 			}
+			else if( value > 100 ){
+				res.send('The numbers all go to eleven. Look, right across the board, eleven, eleven, eleven... Not ' + value + '!');
+			}
+			else if( value < 0 ){
+				res.send('So you like it quiet, eh?');
+			}
 			else {
 				res.send(400, 'Invalid volume value');
 			}
 		break;
 		default:
-			res.send(404, 'Unknown property');
+			res.send(404, 'No hablo americano');
 		break;
 	}
 });
