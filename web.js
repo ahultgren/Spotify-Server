24e308a0fe99e344b0fd0ffcb9baa01d837d7073
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-11-25 20:23:25 +0100

    Created route for creating a room

--- web.js
+++ web.js
@@ -19,15 +19,32 @@ function App(){
 	// Global routing and middleware
 	that.app.use('/static', express.static(__dirname + '/static'));
 	that.app.use(express.cookieParser());
+	that.app.use(express.bodyParser());
 	that.app.use(that.app.router);
 
-	that.app.get('/:username', function(req, res, next){
-		that.rooms.get(req.params.username, function(room){
+	// Create room
+	that.app.post('/:roomname', function(req, res, next){
+		var name = req.params.roomname;
+
+		that.rooms.add({
+			name: name,
+			sio: that.sio,
+			slaveToken: req.body.token,
+			namespaces: {
+				client: '/' + name + '_client',
+				slave: '/' + name + '_slave'
+			}
+		})
+	});
+
+	// Connect to room
+	that.app.get('/:roomname', function(req, res, next){
+		that.rooms.get(req.params.roomname, function(room){
 			room.playerView(req, res, next);
 		},
 		function(){
-			res.send(404, 'Room not found');
-			/*that.users.get(req.params.username, function(user){
+			next();
+			/*that.users.get(req.params.roomname, function(user){
 				user.profileView(req, res, next);
 			},
 			function(){
@@ -36,20 +53,14 @@ function App(){
 		});
 	});
 
+	// Temporary fail handler
+	that.app.use('/:anything', function(req, res){
+		res.send(404, 'No such room found');
+	});
+
 	// Wohoo
 	that.server.listen(port);
 	console.info('Listening on port %s', port);
-
-	// Temporarily immediately set up a route for testing
-	that.rooms.add({
-		name: 'username',
-		sio: that.sio,
-		slaveToken: '1337',
-		namespaces: {
-			client: '/username_client',
-			slave: '/username_slave'
-		}
-	});
 }
 
 new App();
