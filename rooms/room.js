d592df18cefe87752e8c2af8db63329b4f94b612
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-12-11 19:33:43 +0100

    Renamed room views to be a bit more descriptible

--- rooms/room.js
+++ rooms/room.js
@@ -57,10 +57,10 @@ Room.prototype.playerView = function(req, res, next) {
 
 	that.permissions.plainAuth(req.cookies.auth, req.ip, req, function(){
 		if( req.isAuth ){
-			res.sendfile(path.join(__dirname, '/views/index.html'));
+			res.sendfile(path.join(__dirname, '/views/player.html'));
 		}
 		else {
-			res.sendfile(path.join(__dirname, '/views/user.html'));
+			res.sendfile(path.join(__dirname, '/views/viewer.html'));
 		}	
 	});
 };
